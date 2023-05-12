31bafa978ce6301ebb8483ac5b0189d26cab764d
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-05-12 11:04:51 +0200

    Basic fetches from Roles and adding roles to users

--- src/main/role/routes.js
+++ src/main/role/routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAll } from "./controllers.js";
+import { addRoleUser, createRoles, getAll, getUserRoles } from "./controllers.js";
 
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/user_roles/:id", getUserRoles);
 
+router.post("/create_roles", createRoles);
+router.post("/add_role_user", addRoleUser);
 
 export default router;
