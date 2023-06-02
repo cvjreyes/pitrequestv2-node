4f62dc4fd26ee5cbbbccf73d5f99854c00f51279
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-06-02 09:19:16 +0200

    deleting logs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,17 +2,13 @@ import jwt from "jsonwebtoken";
 
 export async function checkAuth(req, res, next) {
   const authHeader = req.headers.authorization;
-  console.log("------------------")
-  console.log("Entra:",authHeader);
   if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
   const token2 = token.split("!").join(".")
   jwt.verify(token2, process.env.NODE_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
-
     req.email = user.email;
     req.roles = user.roles;
-
     next();
   });
 }
